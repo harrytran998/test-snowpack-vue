3a7bf0683af410f33714daf7764c74cec1c82e34
Harry Tran <nhattq.coding@gmail.com>
2020-12-19 11:09:21 +0700

    fix: udpate alias import file

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,21 @@
   <div class="">
     <h1 class="text-red-500">XXX {{ state.message }}</h1>
   </div>
-  <!-- <FooVl /> -->
+  <FooVl />
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive } from 'vue'
-// import FooVl from '@/components/FooVL.vue'
+import FooVl from '@app/components/FooVL.vue'
 
 interface State {
   message: string
 }
 
 export default defineComponent({
-  // components: {
-  //   FooVl,
-  // },
+  components: {
+    FooVl,
+  },
   setup() {
     const state: State = reactive({
       message: 'Learn Vue',
