7ebde2b09305a8443bd853607d5fdb068ccfdfcd
nhat.tran <nhat.tran@encapital.io>
2021-01-06 00:08:02 +0700

    fix: update new version

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="">
-    <h1 class="text-red-500">XXX {{ state.message }}</h1>
+  <h1>Sinh năm 1988 thì năm nay bao nhiêu tuổi?</h1>
+  <div>
+    <input type="text" v-model.number="inputAge" @keyup.enter="checkResult" />
+    <button @click.prevent="checkResult">Kiểm tra</button>
+  </div>
+  <div class="flex place-content-center">
+    <h2>{{ messageResult }}</h2>
+    <video
+      v-if="flagResult"
+      class="w-full h-full"
+      src="https://media.tenor.co/images/df459a6dc6b915ff8a08384ce4bf4e6d/tenor.gif?t=AAW4Kb4RjUcKSp6YHgMMiA&itemid=19253588"
+      autoplay
+      loop
+    />
   </div>
   <FooVl />
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, ref } from 'vue'
 import FooVl from '@app/components/FooVL.vue'
 
 interface State {
@@ -18,12 +30,24 @@ export default defineComponent({
     FooVl,
   },
   setup() {
-    const state: State = reactive({
-      message: 'Learn Vue',
-    })
-
+    const inputAge = ref('')
+    const messageResult = ref('')
+    const flagResult = ref(false)
+    function checkResult() {
+      const binZAge = 2020 - 1988
+      if (+inputAge.value === binZAge) {
+        flagResult.value = true
+        messageResult.value = 'Amazing, good job em 👏'
+      } else {
+        flagResult.value = false
+        messageResult.value = 'Em tính kiểu này thì chết anh rồi ☹️'
+      }
+    }
     return {
-      state,
+      inputAge,
+      flagResult,
+      checkResult,
+      messageResult,
     }
   },
 })
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ app.mount('#app')
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
-// @ts-ignore
 const hrmSnowpack = import.meta.hot
 if (hrmSnowpack) {
   hrmSnowpack.accept()
